952df8c3d893504340bb6e1c461facade8c68948
Evans Omia <evansomia005@gmail.com>
2025-12-06 23:07:05 +0400

    implemented some styles to display the results in an organized way

--- src/components/SearchPage.jsx
+++ src/components/SearchPage.jsx
@@ -19,14 +19,14 @@ export default function SearchPage(){
       setMovies([]);
     }
   };
-  
+
   return(
     <div className="p-6">
       <SearchBar onSearch={handleSearch} />
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+      <div className="p-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {movies.map((movie) => (
           <div key={movie.imdbID} className="bg-white rounded-lg shadow-md overflow-hidden">
-            <img src={movie.Poster !== "N/A" ? movie.Poster : "https://via.placeholder.com/300x450?text=No+Image"} alt={movie.Title} className="w-full h-72 object-cover"/>
+            <img src={movie.Poster !== "N/A" ? movie.Poster : "https://via.placeholder.com/300x450?text=No+Image"} alt={movie.Title} className="w-full h-40 object-cover rounded-xl mb-4"/>
             <div className="p-4">
               <h2 className="text-lg font-semibold mb-2">{movie.Title}</h2>
               <p className="text-gray-600">Year: {movie.Year}</p>
